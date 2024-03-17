380cb9c76cf2ac02bb4c80dbe3ca947d002f6759
indrarizkyy <rizkyindrawibowo@gmail.com>
2024-03-17 12:34:40 +0700

    api get teams

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -14,6 +14,12 @@ class TeamController extends Controller
         return view('admin.team.index', compact('teams'));
     }
 
+    public function teams_show_api() 
+    {
+        $teams = Team::all();
+        return response()->json($teams);
+    }
+
     public function team_search(Request $request)
     {
         $keyword = $request->cari;
